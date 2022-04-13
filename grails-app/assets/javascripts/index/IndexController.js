44e88c056fdd0aa6cdcf5b06d3625da7140d2bc2
Gean Farias <gean.farias@outlook.com>
2022-04-13 20:13:03 -0300

    add validação cpf

--- grails-app/assets/javascripts/index/IndexController.js
+++ grails-app/assets/javascripts/index/IndexController.js
@@ -31,6 +31,12 @@ function IndexController() {
     var bindSubmitForm = function() {
         customerForm.addEventListener("submit", function(eventSubmit) {
             eventSubmit.preventDefault();
+
+            if (!verifyCpf(cpfReference.value.replace(/\D/g, ''))) {
+                alert("CPF inválido");
+                return;
+            }
+
             console.log($(customerForm).serializeArray());
             alert("Formulário enviado com sucesso");
             customerForm.reset();
@@ -72,6 +78,34 @@ function IndexController() {
             }
         })
     };
+
+    var verifyCpf = function(cpf) {
+        var Soma;
+        var Resto;
+        Soma = 0;
+        if (cpf == "00000000000") return false;
+        
+        for (let i=1; i<=9; i++) {
+            Soma = Soma + parseInt(cpf.substring(i-1, i)) * (11 - i);
+        }
+
+        Resto = (Soma * 10) % 11;
+
+        if ((Resto == 10) || (Resto == 11))  Resto = 0;
+        if (Resto != parseInt(cpf.substring(9, 10)) ) return false;
+
+        Soma = 0;
+
+        for (let i = 1; i <= 10; i++) { 
+            Soma = Soma + parseInt(cpf.substring(i-1, i)) * (12 - i);
+        }
+
+        Resto = (Soma * 10) % 11;
+
+        if ((Resto == 10) || (Resto == 11))  Resto = 0;
+        if (Resto != parseInt(cpf.substring(10, 11) ) ) return false;
+        return true;
+    }
 }
 
 var indexController;
